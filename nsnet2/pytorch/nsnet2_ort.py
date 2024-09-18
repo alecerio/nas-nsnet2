8c1e3154b053ad0a9c18366f686e6101e601b1e0
alecerio <alessandrocerioliphd@gmail.com>
2024-09-18 16:28:53 +0200

    nsnet2 model onnxruntime

---
+++ nsnet2/pytorch/nsnet2_ort.py
@@ -0,0 +1,30 @@
+import onnxruntime as ort
+import numpy as np
+
+class NsNetORT():
+    def __init__(self):
+        super(NsNetORT, self).__init__()
+        onnx_model_path = 'nsnet2.onnx'
+        self.session = ort.InferenceSession(onnx_model_path)
+        self.input_names = [input.name for input in self.session.get_inputs()]
+        self.output_name = self.session.get_outputs()[0].name
+    
+    def run(self, input_tensor, h1_tensor, h2_tensor):
+        input_dict = {
+            self.input_names[0]: input_tensor,
+            self.input_names[1]: h1_tensor,     
+            self.input_names[2]: h2_tensor      
+        }
+        output = self.session.run([self.output_name], input_dict)
+        return output
+
+
+
+
+
+#input_tensor = np.ones((1, 1, 257), dtype=np.float32) 
+#h1_tensor = np.ones((1, 1, 400), dtype=np.float32)*2
+#h2_tensor = np.ones((1, 1, 400), dtype=np.float32)
+#nsnet_ort = NsNetORT()
+#output = nsnet_ort.run(input_tensor, h1_tensor, h2_tensor)
+#print(f"Output: {output}")
