dc337f4eb7fc80de4018c594950e298e818f2665
alecerio <alessandrocerioliphd@gmail.com>
2024-09-23 08:36:29 +0200

    quantization biz_2

--- nsnet2/pytorch/nsnet2_quantized.py
+++ nsnet2/pytorch/nsnet2_quantized.py
@@ -54,6 +54,7 @@ class Q_NsNet2_npy(torch.nn.Module):
         self.calib['Whz_2'] = CalibrationParam(8, False, -1.2992678880691528, 1.2991048097610474)
         self.calib['Whr_2'] = CalibrationParam(8, False, -0.8318714499473572, 1.1085889339447021)
         self.calib['Whn_2'] = CalibrationParam(8, False, -0.955470085144043, 1.046797513961792)
+        self.calib['biz_2'] = CalibrationParam(8, False, -0.44805487990379333, 0.1560053527355194)
 
         # weights
 
@@ -114,12 +115,9 @@ class Q_NsNet2_npy(torch.nn.Module):
         self.Whr_2 = self.onnxGRU_205[:,400:800,:]
         self.Whn_2 = self.onnxGRU_205[:,800:,:]
 
-        print(f"min: {np.min(self.Whn_2)}")
-        print(f"max: {np.max(self.Whn_2)}")
         self.Whz_2_q = self._quantize_tensor(self.Whz_2, 'Whz_2')
         self.Whr_2_q = self._quantize_tensor(self.Whr_2, 'Whr_2')
         self.Whn_2_q = self._quantize_tensor(self.Whn_2, 'Whn_2')
-        self._compare(self.Whn_2, self.Whn_2_q, self.calib['Whn_2'])
 
         self.onnxGRU_206 = np.load('onnx__GRU_206.npy')
         self.biz_2 = self.onnxGRU_206[:,:400]
@@ -128,6 +126,11 @@ class Q_NsNet2_npy(torch.nn.Module):
         self.bhz_2 = self.onnxGRU_206[:,1200:1600]
         self.bhr_2 = self.onnxGRU_206[:,1600:2000]
         self.bhn_2 = self.onnxGRU_206[:,2000:]
+
+        print(f"min: {np.min(self.biz_2)}")
+        print(f"max: {np.max(self.biz_2)}")
+        self.biz_2_q = self._quantize_tensor(self.biz_2, 'biz_2')
+        self._compare(self.biz_2, self.biz_2_q, self.calib['biz_2'])
 
         self.onnxMatMul_207 = np.load('onnx__MatMul_207.npy').transpose()
         self.fc2bias = np.load('fc2_bias.npy')
