ee12f8eb47b7d76bab1bc4de36466cfe003e24cb
alecerio <alessandrocerioliphd@gmail.com>
2024-10-07 15:59:54 +0200

    separate mpq configuration

---
+++ nsnet2_c/mpq.h
@@ -0,0 +1,465 @@
+
+#ifndef __MPQ__
+#define __MPQ__
+
+#define X_NBITS 255
+#define X_TYPE uint8_t
+#define X_S (1.8539607843137257e-05)
+#define X_Z (135)
+
+#define FC1_BIAS_NBITS 255
+#define FC1_BIAS_TYPE uint8_t
+#define FC1_BIAS_S (0.0039392154590756285)
+#define FC1_BIAS_Z (124)
+
+#define FC2_BIAS_NBITS 255
+#define FC2_BIAS_TYPE uint8_t
+#define FC2_BIAS_S (0.001229801481845332)
+#define FC2_BIAS_Z (142)
+
+#define WIZ_1_NBITS 255
+#define WIZ_1_TYPE uint8_t
+#define WIZ_1_S (0.003508239517024919)
+#define WIZ_1_Z (123)
+
+#define WIR_1_NBITS 255
+#define WIR_1_TYPE uint8_t
+#define WIR_1_S (0.0024938253795399384)
+#define WIR_1_Z (138)
+
+#define WIN_1_NBITS 255
+#define WIN_1_TYPE uint8_t
+#define WIN_1_S (0.0028225932635513006)
+#define WIN_1_Z (115)
+
+#define WHZ_1_NBITS 255
+#define WHZ_1_TYPE uint8_t
+#define WHZ_1_S (0.01395724287220076)
+#define WHZ_1_Z (132)
+
+#define WHR_1_NBITS 255
+#define WHR_1_TYPE uint8_t
+#define WHR_1_S (0.00857841688043931)
+#define WHR_1_Z (135)
+
+#define WHN_1_NBITS 255
+#define WHN_1_TYPE uint8_t
+#define WHN_1_S (0.006779337864296109)
+#define WHN_1_Z (114)
+
+#define BIZ_1_NBITS 255
+#define BIZ_1_TYPE uint8_t
+#define BIZ_1_S (0.0034234637138890285)
+#define BIZ_1_Z (148)
+
+#define BIR_1_NBITS 255
+#define BIR_1_TYPE uint8_t
+#define BIR_1_S (0.0011188726506981194)
+#define BIR_1_Z (71)
+
+#define BIN_1_NBITS 255
+#define BIN_1_TYPE uint8_t
+#define BIN_1_S (0.0028760029989130355)
+#define BIN_1_Z (193)
+
+#define BHZ_1_NBITS 255
+#define BHZ_1_TYPE uint8_t
+#define BHZ_1_S (0.0037201133428835403)
+#define BHZ_1_Z (143)
+
+#define BHR_1_NBITS 255
+#define BHR_1_TYPE uint8_t
+#define BHR_1_S (0.0008824584238669452)
+#define BHR_1_Z (87)
+
+#define BHN_1_NBITS 255
+#define BHN_1_TYPE uint8_t
+#define BHN_1_S (0.006602613364948946)
+#define BHN_1_Z (119)
+
+#define WIZ_2_NBITS 255
+#define WIZ_2_TYPE uint8_t
+#define WIZ_2_S (0.007259108973484413)
+#define WIZ_2_Z (125)
+
+#define WIR_2_NBITS 255
+#define WIR_2_TYPE uint8_t
+#define WIR_2_S (0.006370335700465184)
+#define WIR_2_Z (150)
+
+#define WIN_2_NBITS 255
+#define WIN_2_TYPE uint8_t
+#define WIN_2_S (0.003756115950790106)
+#define WIN_2_Z (126)
+
+#define WHZ_2_NBITS 255
+#define WHZ_2_TYPE uint8_t
+#define WHZ_2_S (0.01018969685423608)
+#define WHZ_2_Z (128)
+
+#define WHR_2_NBITS 255
+#define WHR_2_TYPE uint8_t
+#define WHR_2_S (0.007609648564282585)
+#define WHR_2_Z (109)
+
+#define WHN_2_NBITS 255
+#define WHN_2_TYPE uint8_t
+#define WHN_2_S (0.00785202980041504)
+#define WHN_2_Z (122)
+
+#define BIZ_2_NBITS 255
+#define BIZ_2_TYPE uint8_t
+#define BIZ_2_S (0.0023688636574090696)
+#define BIZ_2_Z (189)
+
+#define BIR_2_NBITS 255
+#define BIR_2_TYPE uint8_t
+#define BIR_2_S (0.0007888194392709171)
+#define BIR_2_Z (111)
+
+#define BIN_2_NBITS 255
+#define BIN_2_TYPE uint8_t
+#define BIN_2_S (0.0014127153976290835)
+#define BIN_2_Z (170)
+
+#define BHZ_2_NBITS 255
+#define BHZ_2_TYPE uint8_t
+#define BHZ_2_S (0.002213522850298414)
+#define BHZ_2_Z (198)
+
+#define BHR_2_NBITS 255
+#define BHR_2_TYPE uint8_t
+#define BHR_2_S (0.0006787231155470306)
+#define BHR_2_Z (142)
+
+#define BHN_2_NBITS 255
+#define BHN_2_TYPE uint8_t
+#define BHN_2_S (0.0014487537683225147)
+#define BHN_2_Z (119)
+
+#define ONNX__MATMUL_166_NBITS 255
+#define ONNX__MATMUL_166_TYPE uint8_t
+#define ONNX__MATMUL_166_S (0.0016850753157746558)
+#define ONNX__MATMUL_166_Z (131)
+
+#define ONNX__MATMUL_207_NBITS 255
+#define ONNX__MATMUL_207_TYPE uint8_t
+#define ONNX__MATMUL_207_S (0.009898108594557819)
+#define ONNX__MATMUL_207_Z (138)
+
+#define ONNX__MATMUL_208_NBITS 255
+#define ONNX__MATMUL_208_TYPE uint8_t
+#define ONNX__MATMUL_208_S (0.02223231184716318)
+#define ONNX__MATMUL_208_Z (142)
+
+#define ONNX__MATMUL_209_NBITS 255
+#define ONNX__MATMUL_209_TYPE uint8_t
+#define ONNX__MATMUL_209_S (0.01266475425047033)
+#define ONNX__MATMUL_209_Z (103)
+
+#define FC3_BIAS_NBITS 255
+#define FC3_BIAS_TYPE uint8_t
+#define FC3_BIAS_S (0.000752140201774298)
+#define FC3_BIAS_Z (135)
+
+#define FC4_BIAS_NBITS 255
+#define FC4_BIAS_TYPE uint8_t
+#define FC4_BIAS_S (0.0005998921744963702)
+#define FC4_BIAS_Z (178)
+
+#define H1_NBITS 255
+#define H1_TYPE uint8_t
+#define H1_S (2.149590656307398e-05)
+#define H1_Z (123)
+
+#define H2_NBITS 255
+#define H2_TYPE uint8_t
+#define H2_S (2.402609725501023e-05)
+#define H2_Z (129)
+
+#define FC1MATMUL_NBITS 255
+#define FC1MATMUL_TYPE uint8_t
+#define FC1MATMUL_S (1.8245977529411765e-05)
+#define FC1MATMUL_Z (160)
+
+#define FC1ADD_NBITS 255
+#define FC1ADD_TYPE uint8_t
+#define FC1ADD_S (0.00394488257520339)
+#define FC1ADD_Z (124)
+
+#define GRU1_A__NBITS 255
+#define GRU1_A__TYPE uint8_t
+#define GRU1_A__S (0.005531594332526712)
+#define GRU1_A__Z (116)
+
+#define GRU1_A_NBITS 255
+#define GRU1_A_TYPE uint8_t
+#define GRU1_A_S (0.00585017671772078)
+#define GRU1_A_Z (103)
+
+#define GRU1_B__NBITS 255
+#define GRU1_B__TYPE uint8_t
+#define GRU1_B__S (3.5395425762615955e-05)
+#define GRU1_B__Z (139)
+
+#define GRU1_B_NBITS 255
+#define GRU1_B_TYPE uint8_t
+#define GRU1_B_S (0.0008668779450304368)
+#define GRU1_B_Z (86)
+
+#define GRU1_C__NBITS 255
+#define GRU1_C__TYPE uint8_t
+#define GRU1_C__S (0.010241022296980316)
+#define GRU1_C__Z (153)
+
+#define GRU1_C_NBITS 255
+#define GRU1_C_TYPE uint8_t
+#define GRU1_C_S (0.013129275452856925)
+#define GRU1_C_Z (151)
+
+#define GRU1_D__NBITS 255
+#define GRU1_D__TYPE uint8_t
+#define GRU1_D__S (5.8168112574254764e-05)
+#define GRU1_D__Z (145)
+
+#define GRU1_D_NBITS 255
+#define GRU1_D_TYPE uint8_t
+#define GRU1_D_S (0.003796234317854339)
+#define GRU1_D_Z (146)
+
+#define GRU1_E__NBITS 255
+#define GRU1_E__TYPE uint8_t
+#define GRU1_E__S (0.009341012730317958)
+#define GRU1_E__Z (146)
+
+#define GRU1_E_NBITS 255
+#define GRU1_E_TYPE uint8_t
+#define GRU1_E_S (0.011831990877787272)
+#define GRU1_E_Z (157)
+
+#define GRU1_F__NBITS 255
+#define GRU1_F__TYPE uint8_t
+#define GRU1_F__S (4.5353280124711056e-05)
+#define GRU1_F__Z (119)
+
+#define GRU1_F_NBITS 255
+#define GRU1_F_TYPE uint8_t
+#define GRU1_F_S (0.006593195831074435)
+#define GRU1_F_Z (119)
+
+#define GRU1_R__NBITS 255
+#define GRU1_R__TYPE uint8_t
+#define GRU1_R__S (0.0062851337825550755)
+#define GRU1_R__Z (99)
+
+#define GRU1_R_NBITS 255
+#define GRU1_R_TYPE uint8_t
+#define GRU1_R_S (0.0014821220846737131)
+#define GRU1_R_Z (-236)
+
+#define GRU1_Z__NBITS 255
+#define GRU1_Z__TYPE uint8_t
+#define GRU1_Z__S (0.016382201512654623)
+#define GRU1_Z__Z (146)
+
+#define GRU1_Z_NBITS 255
+#define GRU1_Z_TYPE uint8_t
+#define GRU1_Z_S (0.0030296854820905947)
+#define GRU1_Z_Z (-28)
+
+#define GRU1_N1_NBITS 255
+#define GRU1_N1_TYPE uint8_t
+#define GRU1_N1_S (0.003950218827116723)
+#define GRU1_N1_Z (140)
+
+#define GRU1_N2_NBITS 255
+#define GRU1_N2_TYPE uint8_t
+#define GRU1_N2_S (0.013930783552281997)
+#define GRU1_N2_Z (173)
+
+#define GRU1_N_NBITS 255
+#define GRU1_N_TYPE uint8_t
+#define GRU1_N_S (0.00705564302556655)
+#define GRU1_N_Z (139)
+
+#define GRU1_HN1_NBITS 255
+#define GRU1_HN1_TYPE uint8_t
+#define GRU1_HN1_S (0.0030296853944367054)
+#define GRU1_HN1_Z (-47)
+
+#define GRU1_HN2_NBITS 255
+#define GRU1_HN2_TYPE uint8_t
+#define GRU1_HN2_S (0.005315659326665541)
+#define GRU1_HN2_Z (170)
+
+#define GRU1_HN3_NBITS 255
+#define GRU1_HN3_TYPE uint8_t
+#define GRU1_HN3_S (1.3186451613756956e-05)
+#define GRU1_HN3_Z (129)
+
+#define RNN1GRU_NBITS 255
+#define RNN1GRU_TYPE uint8_t
+#define RNN1GRU_S (0.0053186444675221165)
+#define RNN1GRU_Z (170)
+
+#define GRU2_A__NBITS 255
+#define GRU2_A__TYPE uint8_t
+#define GRU2_A__S (0.00624617010939355)
+#define GRU2_A__Z (109)
+
+#define GRU2_A_NBITS 255
+#define GRU2_A_TYPE uint8_t
+#define GRU2_A_S (0.006367312926872104)
+#define GRU2_A_Z (112)
+
+#define GRU2_B__NBITS 255
+#define GRU2_B__TYPE uint8_t
+#define GRU2_B__S (6.338574503566705e-05)
+#define GRU2_B__Z (81)
+
+#define GRU2_B_NBITS 255
+#define GRU2_B_TYPE uint8_t
+#define GRU2_B_S (0.0006690086102953144)
+#define GRU2_B_Z (141)
+
+#define GRU2_C__NBITS 255
+#define GRU2_C__TYPE uint8_t
+#define GRU2_C__S (0.009169421476476333)
+#define GRU2_C__Z (134)
+
+#define GRU2_C_NBITS 255
+#define GRU2_C_TYPE uint8_t
+#define GRU2_C_S (0.01128211255167045)
+#define GRU2_C_Z (149)
+
+#define GRU2_D__NBITS 255
+#define GRU2_D__TYPE uint8_t
+#define GRU2_D__S (6.701340716259153e-05)
+#define GRU2_D__Z (121)
+
+#define GRU2_D_NBITS 255
+#define GRU2_D_TYPE uint8_t
+#define GRU2_D_S (0.0022155718476164574)
+#define GRU2_D_Z (198)
+
+#define GRU2_E__NBITS 255
+#define GRU2_E__TYPE uint8_t
+#define GRU2_E__S (0.005116709307128308)
+#define GRU2_E__Z (132)
+
+#define GRU2_E_NBITS 255
+#define GRU2_E_TYPE uint8_t
+#define GRU2_E_S (0.006347801872328216)
+#define GRU2_E_Z (144)
+
+#define GRU2_F__NBITS 255
+#define GRU2_F__TYPE uint8_t
+#define GRU2_F__S (3.892059857938804e-05)
+#define GRU2_F__Z (105)
+
+#define GRU2_F_NBITS 255
+#define GRU2_F_TYPE uint8_t
+#define GRU2_F_S (0.0014450010715746412)
+#define GRU2_F_Z (119)
+
+#define GRU2_R__NBITS 255
+#define GRU2_R__TYPE uint8_t
+#define GRU2_R__S (0.006318024794260661)
+#define GRU2_R__Z (117)
+
+#define GRU2_R_NBITS 255
+#define GRU2_R_TYPE uint8_t
+#define GRU2_R_S (0.0014979704922320797)
+#define GRU2_R_Z (-215)
+
+#define GRU2_Z__NBITS 255
+#define GRU2_Z__TYPE uint8_t
+#define GRU2_Z__S (0.013065239962409525)
+#define GRU2_Z__Z (162)
+
+#define GRU2_Z_NBITS 255
+#define GRU2_Z_TYPE uint8_t
+#define GRU2_Z_S (0.002603292435991998)
+#define GRU2_Z_Z (-41)
+
+#define GRU2_N1_NBITS 255
+#define GRU2_N1_TYPE uint8_t
+#define GRU2_N1_S (0.0006832725277134017)
+#define GRU2_N1_Z (139)
+
+#define GRU2_N2_NBITS 255
+#define GRU2_N2_TYPE uint8_t
+#define GRU2_N2_S (0.006728582989935781)
+#define GRU2_N2_Z (150)
+
+#define GRU2_N_NBITS 255
+#define GRU2_N_TYPE uint8_t
+#define GRU2_N_S (0.005388977013382258)
+#define GRU2_N_Z (142)
+
+#define GRU2_HN1_NBITS 255
+#define GRU2_HN1_TYPE uint8_t
+#define GRU2_HN1_S (0.002603292465209961)
+#define GRU2_HN1_Z (-88)
+
+#define GRU2_HN2_NBITS 255
+#define GRU2_HN2_TYPE uint8_t
+#define GRU2_HN2_S (0.004055405130573348)
+#define GRU2_HN2_Z (168)
+
+#define GRU2_HN3_NBITS 255
+#define GRU2_HN3_TYPE uint8_t
+#define GRU2_HN3_S (1.3741757720708847e-05)
+#define GRU2_HN3_Z (119)
+
+#define RNN2GRU_NBITS 255
+#define RNN2GRU_TYPE uint8_t
+#define RNN2GRU_S (0.004055991944144754)
+#define RNN2GRU_Z (168)
+
+#define FC2MATMUL_NBITS 255
+#define FC2MATMUL_TYPE uint8_t
+#define FC2MATMUL_S (0.004723190209444832)
+#define FC2MATMUL_Z (163)
+
+#define FC2ADD_NBITS 255
+#define FC2ADD_TYPE uint8_t
+#define FC2ADD_S (0.005743133086784214)
+#define FC2ADD_Z (162)
+
+#define RELU_NBITS 255
+#define RELU_TYPE uint8_t
+#define RELU_S (0.0020899260745329017)
+#define RELU_Z (0)
+
+#define FC3MATMUL_NBITS 255
+#define FC3MATMUL_TYPE uint8_t
+#define FC3MATMUL_S (0.014787528795354507)
+#define FC3MATMUL_Z (194)
+
+#define FC3ADD_NBITS 255
+#define FC3ADD_TYPE uint8_t
+#define FC3ADD_S (0.014849604812322878)
+#define FC3ADD_Z (195)
+
+#define RELU_1_NBITS 255
+#define RELU_1_TYPE uint8_t
+#define RELU_1_S (0.0035128163356407016)
+#define RELU_1_Z (0)
+
+#define FC4MATMUL_NBITS 255
+#define FC4MATMUL_TYPE uint8_t
+#define FC4MATMUL_S (0.006203068003934972)
+#define FC4MATMUL_Z (413)
+
+#define FC4ADD_NBITS 255
+#define FC4ADD_TYPE uint8_t
+#define FC4ADD_S (0.006303162434521843)
+#define FC4ADD_Z (408)
+
+#define SIGMOID_NBITS 255
+#define SIGMOID_TYPE uint8_t
+#define SIGMOID_S (0.000805279203489715)
+#define SIGMOID_Z (-88)
+
+#endif // __MPQ__
