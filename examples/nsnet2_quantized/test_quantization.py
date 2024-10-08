ece567cf9c53e35ee5cea4505de357920b53f12d
alecerio <alessandrocerioliphd@gmail.com>
2024-10-08 13:10:13 +0200

    update nsnet quantized example with inference time benchmark

--- examples/nsnet2_quantized/test_quantization.py
+++ examples/nsnet2_quantized/test_quantization.py
@@ -1,5 +1,6 @@
 from nsnet2.pytorch.nsnet2.nsnet2 import NsNet2_npy
 from nsnet2.pytorch.nsnet2_q.nsnet2_quantized import Q_NsNet2_npy
+from objective_function.inference_time.compute_inference_time import compute_inference_time
 import numpy as np
 import torch
 
@@ -15,3 +16,8 @@ y = baseline(x, h1, h2)
 mpq_config = np.ones(93)*8
 quantized = Q_NsNet2_npy(numpy_weights_path, mpq_config)
 yq = quantized(x, h1, h2)
+
+root_path = '/home/alessandro/Desktop/nas-nsnet2/'
+build_path = '/home/alessandro/Desktop/build_nsnet2_nas/'
+inference_time = compute_inference_time(quantized.calib, root_path, build_path)
+print(inference_time)
